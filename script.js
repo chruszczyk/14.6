fc33f71647a927789fb93b011fdbb34035dfb7a7
Alicja Chruszczyk <alicja.chruszczyk@gmail.com>
2018-06-17 12:38:36 +0200

    fix script

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-var element1 = React.createElement(Counter1);
-var element2 = React.createElement(Counter2);
-var element3 = React.createElement(Counter3);
-ReactDOM.render(element1, document.getElementById('app1'));
-ReactDOM.render(element2, document.getElementById('app2'));
-ReactDOM.render(element3, document.getElementById('app3'));
+var componentsCounter = 0;
+
+var Counter = React.createClass({
+
+	propTypes: {
+		id: React.PropTypes.number.isRequired,
+	},
+
+	getInitialState: function(){
+		return {
+			counter: 0
+		};
+	},
+
+	increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+
+	decrement: function() {
+		this.setState({
+			counter: this.state.counter - 1
+		})
+	},
+
+render: function() {
+		return React.createElement('div', {className: 'divCounter'}, 
+				React.createElement('button', {onClick: this.decrement}, '-'),
+				React.createElement('div', {className: 'counter'}, this.state.counter),
+				React.createElement('button', {onClick: this.increment}, '+'),
+			)
+	}
+});
+
+var element = React.createElement('div', {}, 
+	React.createElement(Counter, {'id' : componentsCounter++ }),
+	React.createElement(Counter, {'id' : componentsCounter++ }),
+	React.createElement(Counter, {'id' : componentsCounter++ }),
+);
+
+ReactDOM.render(element, document.getElementById('app'));
